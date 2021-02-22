fcf0cfd7ca72b0b8b5ce4be55c56ffcc347c4c1a
Anais Francois <anaisfrncs@gmail.com>
2021-02-22 17:05:52 +0100

    validates model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :comments
+  has_many :users_votes
+  validates :email, :password, :first_name, :last_name, :city, :address, :age, :zip, presence: true
+  validates :zip, :age, numericality: { only_integer: true }
 end
