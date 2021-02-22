380ed9bda9e5066d240fb0b2c08b69b433a3c8cb
Maxime Cazes <maxime.cazes@essca.eu>
2021-02-22 17:45:27 +0100

    validations done

--- app/models/law.rb
+++ app/models/law.rb
@@ -1,2 +1,9 @@
 class Law < ApplicationRecord
+  has_many :representatives_votes
+  has_many :users_votes
+  has_many :comments
+
+  validates :num, presence: true
+  validates :title, presence: true
+  validates :description, presence: true
 end
--- app/models/representative.rb
+++ app/models/representative.rb
@@ -1,2 +1,16 @@
 class Representative < ApplicationRecord
+  CIV = ["Mme.", "M."]
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :salutation, presence: true, inclusion: { with: CIV }
+  validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  validates :circonscription_ref, presence: true, numericality: { only_integer: true }
+  validates :circonscription_num, presence: true, numericality: { only_integer: true }
+  validates :city, presence: true
+  validates :department, presence: true
+  validates :region, presence: true
+  validates :representative_ref, presence: true, numericality: { only_integer: true }
+  validates :party, presence: true
+  validates :first_election, presence: true
+  validates :hemicycle_seat, presence: true
 end
