ec4601df66452123c80d0a95678343e5ff5230b1
Maxime Cazes <maxime.cazes@essca.eu>
2021-02-23 12:23:51 +0100

    did view and controller for rep, repv and law

--- app/controllers/laws_controller.rb
+++ app/controllers/laws_controller.rb
@@ -1,2 +1,9 @@
 class LawsController < ApplicationController
+  def index
+    @laws = Law.all
+  end
+
+  def show
+    @law = Law.find(params[:id])
+  end
 end
--- app/controllers/representatives_votes_controller.rb
+++ app/controllers/representatives_votes_controller.rb
@@ -1,2 +1,16 @@
 class RepresentativesVotesController < ApplicationController
+  def new
+    @representative_vote = RepresentativesVote.new
+  end
+
+  def create
+    @representative_vote = RepresentativesVote.new(vote_params)
+    @representative_vote.save
+  end
+
+  private
+
+  def vote_params
+    params.require(:representatives_vote).permit(:voting_status)
+  end
 end
