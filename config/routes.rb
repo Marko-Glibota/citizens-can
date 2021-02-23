4e7fe01340d8b3abac363899471b63ddfe3ebded
Anais Francois <anaisfrncs@gmail.com>
2021-02-23 11:03:12 +0100

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :representative, only: [:show] do
+    resources :reprensatives_votes, only: [:new, :create]
+  end
+  resources :law, only: [:index, :show] do
+    resources :comment, only: [:new, :create]
+    resources :users_votes, only: [:new, :create]
+  end
 end
